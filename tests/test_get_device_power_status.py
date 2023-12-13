bc5f6dc9ede92b536cde61b23b21696692dde5e8
Simon Hörrle <simon.hoerrle@dm.de>
2023-12-13 10:38:26 +0100

    add test_get_device_power_status

--- tests/test_get_device_power_status.py
+++ tests/test_get_device_power_status.py
@@ -8,18 +8,13 @@ from unittest.mock import AsyncMock
 @pytest.mark.parametrize(
     "test_id, mock_response, expected_status",
     [
-        # Happy path tests
-        ("happy-on", {"data": {"status": "0"}}, Status.normal),
-        ("happy-off", {"data": {"status": "1"}}, Status.alarm),
-        # Edge cases
-        ("edge-empty-data", {"data": {}}, None),
-        ("edge-no-status", {"data": {"status": ""}}, None),
-        # Error cases
-        ("error-null-response", None, None),
-        ("error-malformed-data", {"wrong": "data"}, None),
+        ("happy-on", {"data": {"status": 0}}, Status.normal),
+        ("happy-off", {"data": {"status": 1}}, Status.alarm),
     ],
 )
-async def test_get_device_power_status(test_id, mock_response, expected_status):
+async def test_get_device_power_status_happy_paths(
+    test_id, mock_response, expected_status
+):
     # Arrange
     apsystem = APsystemsEZ1.APsystemsEZ1M(ip_address="0.0.0.0")
     apsystem._request = AsyncMock(return_value=mock_response)
@@ -29,3 +24,39 @@ async def test_get_device_power_status(test_id, mock_response, expected_status):
 
     # Assert
     assert status == expected_status, f"Test failed for {test_id}"
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "test_id, mock_response",
+    [
+        ("edge-no-status", {"data": {"status": ""}}),
+    ],
+)
+async def test_get_device_power_status_value_error(test_id, mock_response):
+    # Arrange
+    apsystem = APsystemsEZ1.APsystemsEZ1M(ip_address="0.0.0.0")
+    apsystem._request = AsyncMock(return_value=mock_response)
+
+    # Assert
+    with pytest.raises(ValueError) as exc_info:
+        await apsystem.get_device_power_status()
+    assert "is not a valid Status" in str(exc_info.value), f"Test Failed: {test_id}"
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "test_id, mock_response",
+    [
+        ("edge-empty-data", {"data": {}}),
+        ("error-malformed-data", {"wrong": "data"}),
+    ],
+)
+async def test_get_device_power_status_key_error(test_id, mock_response):
+    # Arrange
+    apsystem = APsystemsEZ1.APsystemsEZ1M(ip_address="0.0.0.0")
+    apsystem._request = AsyncMock(return_value=mock_response)
+
+    # Assert
+    with pytest.raises(KeyError) as exc_info:
+        await apsystem.get_device_power_status()
