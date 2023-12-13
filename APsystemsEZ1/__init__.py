e1d768712e8aa412aa7d7d7cae0e91a39b0ab57e
Simon Hörrle <simon.hoerrle@dm.de>
2023-12-13 10:24:06 +0100

    dont use mapping for non str value

--- APsystemsEZ1/__init__.py
+++ APsystemsEZ1/__init__.py
@@ -235,7 +235,7 @@ class APsystemsEZ1M:
         :return: 0/normal when on, 1/alarm when off
         """
         response = await self._request("getOnOff")
-        return Status(**response["data"]["status"]) if response else None
+        return Status(response["data"]["status"]) if response else None
 
     async def set_device_power_status(self, power_status) -> Status | None:
         """
